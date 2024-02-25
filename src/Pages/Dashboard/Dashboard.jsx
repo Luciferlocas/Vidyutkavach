f8a7abe3bb010fd30ac00b54fc802446ea31b5e0
Lucifer Locas <rohitsomvanshi2003@gmail.com>
2024-02-25 16:24:41 +0530

    removed unwanted import

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,4 @@
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Divider,
-  Link,
-  Image,
-} from "@nextui-org/react";
 
 const Dashboard = () => {
   return (
